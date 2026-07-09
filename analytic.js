5592c5e4207bca884acf0a2c6523c9d1e14b484b
winnecinc1-jpg <winnecinc1@gmail.com>
2026-07-09 11:21:13 -0400

    Save local changes

--- analytic.js
+++ analytic.js
@@ -1,9 +1,18 @@
-// analytics.js
+(function () {
+  const GA_ID = "G-4N3LN3MG1M";
 
-window.dataLayer = window.dataLayer || [];
-window.gtag = function () {
-  dataLayer.push(arguments);
-};
+  const s1 = document.createElement("script");
+  s1.async = true;
+  s1.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID;
+  document.head.appendChild(s1);
 
-gtag('js', new Date());
-gtag('config', 'G-4N3LN3MG1M');
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () {
+    dataLayer.push(arguments);
+  };
+
+  s1.onload = function () {
+    gtag("js", new Date());
+    gtag("config", GA_ID);
+  };
+})();
