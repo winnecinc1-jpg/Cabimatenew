6eb8a4222b3b58ae9a978a4065708ad90f7cef72
winnecinc1-jpg <winnecinc1@gmail.com>
2026-07-24 16:18:24 -0400

    Save local changes

--- shopify-products.js
+++ shopify-products.js
@@ -1,68 +1,36 @@
 const SHOPIFY_DOMAIN = "winnecinc.myshopify.com";
+const TOKEN = "atkn_f81d1eb3e66f2c3427354af92654598daad17d4a83942c4a2eb9bcd36e1430dd";
+console.log("Token length:", TOKEN.length);
 
-const TOKEN = "YOUR_STOREFRONT_TOKEN";
+console.log("Shopify script loaded");
 
 fetch(`https://${SHOPIFY_DOMAIN}/api/2025-07/graphql.json`, {
-method: "POST",
-
-headers: {
-"Content-Type": "application/json",
-"X-Shopify-Storefront-Access-Token": TOKEN
-},
-
-body: JSON.stringify({
-
-query: `
-{
-products(first:5){
-edges{
-node{
-title
-featuredImage{
-url
-}
-}
-}
-}
-}
-`
-
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "X-Shopify-Storefront-Access-Token": TOKEN
+  },
+  body: JSON.stringify({
+    query: `
+    {
+      products(first:5){
+        edges{
+          node{
+            title
+          }
+        }
+      }
+    }
+    `
+  })
 })
-
+.then(response => {
+  console.log("Status:", response.status);
+  return response.json();
 })
-
-.then(response => response.json())
-
 .then(result => {
-
-console.log(result);
-
-let box = document.getElementById("shopify-products");
-
-box.innerHTML = "";
-
-result.data.products.edges.forEach(item=>{
-
-let product=item.node;
-
-box.innerHTML += `
-
-<div style="width:250px">
-
-<img src="${product.featuredImage.url}" style="width:100%">
-
-<h3>${product.title}</h3>
-
-</div>
-
-`;
-
-});
-
+  console.log("Shopify result:", result);
 })
-
-.catch(error=>{
-
-console.log("ERROR:", error);
-
+.catch(error => {
+  console.log("Fetch error:", error);
 });
